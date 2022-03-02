ff13b1595ac44ac34d0275752829e5f86183a750
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-03-02 12:55:35 +0700

    m

--- view/header.php
+++ view/header.php
@@ -6,15 +6,24 @@
 	<title>Công ty</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="icon" type="image/gif/png" href="./images/logo/logo.png">
+	<link rel="icon" type="image/gif/png" href="/thuctap/view/images/logo/logo.png">
 </head>
 <body>
 	<div class="container">
+
 		<header class="header">
+
 			<div class="header__logo">
-				<div><img src="../logo.png" alt=""></div>
+				<div class="logo">
+
+					<img src="/thuctap/view/images/logo/logo.png" alt="">
+
+				</div>
 			</div>
+
 			<div class="header__navbar"></div>
+
 			<div class="header__slide"></div>
 		</header>
+
 	</div>

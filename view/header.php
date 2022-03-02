b0c3a28cfdb76f4ad754c3a1a362bf6916009da7
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-03-02 13:11:14 +0700

    update file main.css for index.php

--- view/header.php
+++ view/header.php
@@ -7,23 +7,40 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="icon" type="image/gif/png" href="/thuctap/view/images/logo/logo.png">
+	<link rel="stylesheet" href="/thuctap/view/css/main.css">
 </head>
 <body>
 	<div class="container">
-
+		<!-- header -->
 		<header class="header">
 
 			<div class="header__logo">
+
 				<div class="logo">
 
-					<img src="/thuctap/view/images/logo/logo.png" alt="">
+					<img src="/thuctap/view/images/logo/logo.png" class="logo__image"alt="">
 
 				</div>
+
+				<div class="button button-active">
+
+					<button>
+
+						<i class="fa-solid fa-bars"></i>
+
+					</button>
+
+				</div>
+
 			</div>
 
 			<div class="header__navbar"></div>
 
 			<div class="header__slide"></div>
 		</header>
-
-	</div>
+		<!-- end header -->
+		
+		
+<!-- 	</div>
+</body>
+</html> -->
